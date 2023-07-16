cc996d7496db45dc215e713e49a2df3a89b15c85
SimoneAncona <simone.ancona00@gmail.com>
2023-07-16 17:43:35 +0200

    ptools

---
+++ include/ptools.hh
@@ -0,0 +1,15 @@
+/**
+ * @file ptools.hh
+ * @author Simone Ancona
+ * @brief Parser Tools
+ * @version 1.0
+ * @date 2023-07-16
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+namespace ParserTools
+{
+
+};
---
+++ src/ptools.cc
@@ -0,0 +1,11 @@
+/**
+ * @file ptools.cc
+ * @author Simone Ancona
+ * @version 1.0
+ * @date 2023-07-16
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "ptools.hh"
